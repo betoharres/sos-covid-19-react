74409ca4036f90c24ac36421e33e7174b355b3b5
Alberto Rocha <betoharres@gmail.com>
2020-03-27 17:22:21 -0300

    feat: add google maps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import GoogleMapReact from 'google-map-react'
+
+import { MapView } from './App.styles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapView>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
+        defaultCenter={{
+          lat: 59.95,
+          lng: 30.33,
+        }}
+        defaultZoom={10}
+      />
+    </MapView>
   )
 }
 
