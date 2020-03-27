51e4f882405f953b815519f8cae6e0427f65014d
Alberto Rocha <betoharres@gmail.com>
2020-03-27 17:47:06 -0300

    feat: add useLocation custom hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useCallback } from 'react'
+
+export default function useLocation({
+  latitude,
+  longitude,
+  altitude,
+  timeout = 15000,
+  maximumAge = 0,
+  enableHighAccuracy = true,
+  onError = Function.prototype,
+} = {}) {
+  const [error, setError] = useState(null)
+  const [currentLocation, setCurrentLocation] = useState(() => {
+    if (
+      !(
+        isNaN(Number(latitude)) &&
+        isNaN(Number(longitude)) &&
+        isNaN(Number(altitude))
+      )
+    ) {
+      return { latitude, longitude, altitude }
+    } else {
+      return {}
+    }
+  })
+
+  const hasLocation = !(
+    isNaN(currentLocation.latitude) && isNaN(currentLocation.longitude)
+  )
+
+  const getCurrentPosition = useCallback(
+    (options) =>
+      new Promise((resolve, reject) =>
+        navigator.geolocation.getCurrentPosition(resolve, reject, {
+          timeout,
+          maximumAge,
+          enableHighAccuracy,
+          ...options,
+        })
+      ),
+    [timeout, maximumAge, enableHighAccuracy]
+  )
+
+  const updateLocation = useCallback(async () => {
+    try {
+      const { coords } = await getCurrentPosition()
+      setCurrentLocation(coords)
+      setError(null)
+      return coords
+    } catch (err) {
+      setError(err)
+    }
+  }, [getCurrentPosition])
+
+  useEffect(() => {
+    if (!hasLocation) {
+      updateLocation()
+    }
+  }, [currentLocation, hasLocation, updateLocation])
+
+  return {
+    currentLocation,
+    setCurrentLocation,
+    getCurrentPosition,
+    updateLocation,
+    hasLocation,
+    error,
+  }
+}
