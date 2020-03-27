3fffe6df571e9f4ac35d25336d0f5311b697c416
Alberto Rocha <betoharres@gmail.com>
2020-03-27 17:47:23 -0300

    feat: improve gmap config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 
+// import { useLocation } from './hooks'
 import { MapView } from './App.styles'
 
 function App() {
+  const mapRef = useRef()
+  const [zoom, setZoom] = useState(10)
+  const [, setBounds] = useState(null)
+  const defaultCenter = { lat: 59.95, lng: 30.33 }
+
+  function onGoogleApiLoaded({ map, maps }) {
+    mapRef.current = map
+  }
+
+  function onChangeMap({ zoom, bounds }) {
+    setZoom(zoom)
+    setBounds([bounds.nw.lng, bounds.se.lat, bounds.se.lng, bounds.nw.lat])
+  }
+
   return (
     <MapView>
       <GoogleMapReact
+        yesIWantToUseGoogleMapApiInternals
         bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
-        defaultCenter={{
-          lat: 59.95,
-          lng: 30.33,
-        }}
-        defaultZoom={10}
+        defaultCenter={defaultCenter}
+        defaultZoom={zoom}
+        onGoogleApiLoaded={onGoogleApiLoaded}
+        onChange={onChangeMap}
       />
     </MapView>
   )
