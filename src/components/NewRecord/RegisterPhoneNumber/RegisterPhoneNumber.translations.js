114b292c188a57e09fd712623cb91c92a6f6c445
Alberto Rocha <betoharres@gmail.com>
2020-05-12 21:49:13 -0300

    update: RegisterPhoneNumber caption text

--- src/components/NewRecord/RegisterPhoneNumber/RegisterPhoneNumber.translations.js
+++ src/components/NewRecord/RegisterPhoneNumber/RegisterPhoneNumber.translations.js
@@ -3,11 +3,10 @@ import i18 from 'i18next'
 
 const pt = {
   Details: `\
-    Seu número de celular é obrigatório para que o registro seja válido no sistema.
+    Seu número de celular é obrigatório para que o registro seja válido.
     Se o número for válido e tiver sinal da operadora, você irá receber um SMS com
-    um código numérico de 4 dígitos para inserir, validando assim o registro.
+    um código numérico de 4 dígitos para inserir na janela que irá abrir no site.
     O registro será apagado caso o número não seja confirmado em até 30 minutos.
-    Você pode enviar seus sintomas novamente após esse período se desejar.
   `,
 }
 
